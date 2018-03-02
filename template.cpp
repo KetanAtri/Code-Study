fbb86a1ab7a8179cd8e489e90f338d670b2adfe0
KetanAtri <atriketan@gmail.com>
2018-03-02 18:41:05 +0530

    Adding template file

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f(q, a, b) for(int q = int(a); q<int(b); q++)
+#define fb(q, a, b) for(int q = int(a); q>int(b); q--)
+#define pb(v, x) v.push_back(x)
+#define A first
+#define B second
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+
+//ifstream in("input");
+//ofstream out("output");
+
+int main()
+{
+    /*int t;
+    cin>>t;
+    while(t--){
+    }*/
+}
